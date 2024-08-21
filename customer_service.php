a34c2c101281b1e031130d0d1cd1eb77611c1690
shselbst <93411312+shselbst@users.noreply.github.com>
2024-08-21 13:48:33 -0400

    Create customer_service.php
    customer service page of organization website

---
+++ customer_service.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <img src="https://55-org-marketing1.s3.amazonaws.com/telescopelogo.jpg" style="width:800px">
+</head>
+<body>
+    <h1>Customer Service Page</h1>
+    <p>Hello! If you are experiencing technical difficulties, have questions about a product, or have any other related troubles or complaints, please call this number: <s>
+    <h2>Help Ticket</h2>
+    <form action="submit_ticket.php" method="post">
+        <p>
+            <label for="email">Email address:</label><br>
+            <input type="email" id="email" name="email" required>
+        </p>
+        <p>
+            <label for="issue">Describe the issue here:</label><br>
+            <textarea id="issue" name="issue" rows="5" cols="50" required></textarea>
+        </p>
+        <p>
+            <input type="submit" value="Submit">
+        </p>
+    </form>
+</body>
+</html>
