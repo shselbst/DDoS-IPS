eef2b0ffbc95934e9a81792149ddac4b1deca59f
shselbst <93411312+shselbst@users.noreply.github.com>
2024-08-25 20:46:34 -0400

    Update xss_sol.sh
    added comments

--- xss_sol.sh
+++ xss_sol.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# solution for .php files to prevent cross-site scripting. looks for variables in .php and surrounds them in htmlspecialchars() so that script tags are not rendered
+# USE: sudo ./xss_sol.sh input_file.php output_file.php
+
 
 # check if we have been given the correct amount of parameters
 if [ "$#" -ne 2 ]; then
@@ -17,7 +20,7 @@ touch "$output_file"
 # check if the input file exists and exit with error if not
 if [ ! -f "$input_file" ]; then
     echo "Input file does not exist."
-    exit 1
+    exit 1 # exit with eror
 fi
 
 # usee sed to surround any calls to a variable from a list (like superglobal array like POST  or grabbing from an array of inputs] with htmlspecialchars()
